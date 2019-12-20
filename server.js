d676dab3902c6a0b281641ceca65a886e7e7a144
Sufyan Jami <sufijami9@gmail.com>
2019-12-20 10:26:01 -0700

    Added

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
     extended: false
 })); // support encoded bodies
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// })
 app.use(bodyParser.json()); // support json encoded bodies
 ​
 //set port
@@ -22,3 +22,4 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
+
